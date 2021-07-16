911e991242a0c84a689565762cf756fbf52299ca
ThulayS <84454542+ThulayS@users.noreply.github.com>
2021-07-16 23:16:15 +0200

    Add files via upload

---
+++ JSopdracht5/index.js
@@ -0,0 +1,23 @@
+let person = {
+    name: "Myrna",
+    age: 33
+}
+
+let evaluations = [7, 10, 9]
+
+let selectedColors = ["Groen", "Blauw", "Rood"]
+selectedColors.push("Yellow");
+selectedColors.push(1)
+selectedColors.push("Greeting: Hi ik ben een object")
+
+console.log(person.age)
+
+console.log(evaluations)
+
+console.log(selectedColors)
+
+console.log(selectedColors.length)
+
+console.log(selectedColors[0])
+
+console.log(selectedColors[selectedColors.length - 1])
