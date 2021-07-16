e30baa5454175e5df241a94ccb59f1783394f24d
ThulayS <84454542+ThulayS@users.noreply.github.com>
2021-07-16 15:27:34 +0200

    Add files via upload

---
+++ JSopdracht2/script.js
@@ -0,0 +1,6 @@
+console.log("Hello Winc Academy Studenten");
+// dit is een grote som
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:", division);
