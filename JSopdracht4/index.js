17b204c33d09fffdc9df3e3ea90db20e9c3042f5
ThulayS <84454542+ThulayS@users.noreply.github.com>
2021-07-16 21:55:55 +0200

    Add files via upload

---
+++ JSopdracht4/index.js
@@ -0,0 +1,36 @@
+const myAge = 26;
+
+if (myAge >= 18 && myAge <= 25) {
+
+    console.log("Je krijgt 50% korting");
+
+} else {
+
+    console.log("Dit is leeftijdsdicriminatie");
+}
+
+const firstName = "Sander"
+
+if (firstName === "Bram" || firstName === "Sarah") {
+
+    console.log("Gratis biertje voor jou!");
+
+} else {
+
+    console.log("Vier een feestje samen met Sarah en Bram!")
+}
+
+const totalAmount = "90"
+
+if (totalAmount >= 25 && totalAmount < 50) {
+
+    console.log("Gratis bitterballen!");
+
+} else if (totalAmount >= 50 && totalAmount < 100) {
+
+    console.log("Gratis portie nachos!");
+
+} else if (totalAmount > 100) {
+
+    console.log("Gratis fles Champagne");
+}
