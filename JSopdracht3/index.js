54e293f6a4feed04ee9c1decf25964fdb95b1286
ThulayS <84454542+ThulayS@users.noreply.github.com>
2021-07-16 17:11:23 +0200

    Add files via upload

---
+++ JSopdracht3/index.js
@@ -0,0 +1,32 @@
+const myAge = 17;
+
+if (myAge > 17) {
+
+    console.log("Welkom, kom binnen");
+
+} else {
+
+    console.log("Helaas, geen toegang");
+}
+
+const isFemale = true;
+
+if (isFemale) {
+
+    console.log("Ladies Night!!");
+
+} else {
+
+    console.log(".... waarom niet ....??? ");
+
+}
+
+const driverStatus = true;
+if (driverStatus) {
+
+    console.log("Je bent de Bob");
+
+} else {
+
+    console.log("Geen Bob hier");
+}
