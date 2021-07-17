96b8a32becd77e63f184874bb5450883427dfe54
ThulayS <84454542+ThulayS@users.noreply.github.com>
2021-07-17 22:37:45 +0200

    JSopdracht6

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const makePizza = function () {
+    console.log("Make Dough");
+    console.log("Let dough rise;");
+    console.log("Make sauce");
+    console.log("Get toppings");
+    console.log("Combine dough sauce and toppings");
+    console.log("Bake pizza")
+};
+
+const makeSushi = function () {
+    console.log("Make rice");
+    console.log("Prepare toppings");
+    console.log("prepare seaweed");
+    console.log("Roll sushi");
+};
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
