61faaedf8546a7154c38d7300749178f355b8de5
ThulayS <84454542+ThulayS@users.noreply.github.com>
2021-07-18 22:09:23 +0200

    Add files via upload

---
+++ JSopdracht12/opdr12.js
@@ -0,0 +1,15 @@
+let color = ("yellow", "blue", "red", "orange")
+
+for (let i = 1; i < color.length; i++) {
+    console.log(i);
+}
+
+// array.each
+
+const array1 = ['yellow', 'blue', 'red', 'orange'];
+
+array1.forEach(element => console.log(element));
+
+//  ik kom er met de while/for niet helemaal uit qua kleuren. die array.foreach is veel logischer
+// om te gebruiken en dat is ook de enige manier die ik terug kan vinden
+// met cijfers is for geen probleem/ logisch voor mij.
