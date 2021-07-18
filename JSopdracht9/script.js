15e76129b37f5735616c957b979b72d6617a0459
ThulayS <84454542+ThulayS@users.noreply.github.com>
2021-07-18 16:51:02 +0200

    Add files via upload

---
+++ JSopdracht9/script.js
@@ -0,0 +1,18 @@
+// function square(number1, number2) {
+//     return number1 + number2;
+// }
+
+// console.log(square(5));
+
+const square = function (number1) {
+
+    return number1 * number1;
+};
+
+console.log(square(5));
+
+
+// const square = (number1) => {
+//     return number1 * number1;
+// };
+// console.log(square(5));
